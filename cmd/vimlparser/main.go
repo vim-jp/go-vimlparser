af373c24b6f8cbe24c87a5e2ca35bce15c9cd498
haya14busa <hayabusa1419@gmail.com>
2016-09-18 02:29:56 +0900

    do not use log.Fatal

--- cmd/vimlparser/main.go
+++ cmd/vimlparser/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
 	"os"
 
 	"github.com/haya14busa/go-vimlparser"
@@ -19,18 +19,21 @@ func main() {
 	if p := flag.Arg(0); p != "" {
 		f, err := os.Open(p)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Fprintln(os.Stderr, err)
 			flag.Usage()
+			os.Exit(1)
 		}
 		r = f
 	}
 
 	node, err := vimlparser.ParseFile(r, &vimlparser.ParseOption{Neovim: *neovim})
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 	c := &compiler.Compiler{Config: compiler.Config{Indent: "  "}}
 	if err := c.Compile(os.Stdout, node); err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
