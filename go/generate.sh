e057c6c220c37edf175a7da5438e0015d19efc5c
haya14busa <haya14busa@gmail.com>
2017-11-17 16:24:54 +0000

    Add go/gen_builtin_commands.vim
    Also, run gofmt.

--- go/generate.sh
+++ go/generate.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
-vim -u NONE -N --cmd "let &rtp .= ',' . getcwd()" -S go/generate.vim
+#!/bin/bash
+vim -u NONE -N --cmd "let &rtp .= ',' . getcwd()" -S go/generate.vim -c ":q"
+vim -u NONE -N --cmd "let &rtp .= ',' . getcwd()" -S go/gen_builtin_commands.vim -c ":q"
+gofmt -s -w go/*.go
 
